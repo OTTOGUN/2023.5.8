377b0ac8303bc4af1ca8841fee4ba5e2fbfd719b
OTTOGUN <127713240+OTTOGUN@users.noreply.github.com>
2023-05-08 22:50:33 +0800

    Add files via upload

---
+++ Linux.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int i,j,k;
+    printf("\n");
+    for (int i = 1; i < 5; ++i) {
+        for (int j = 1; j < 5; ++j) {
+            for (int k = 1; k < 5; ++k) {
+                if(i != k && i != j && j != k){
+                    printf("%d,%d,%d\n",i,j,k);
+                }
+            }
+        }
+    }
+}
